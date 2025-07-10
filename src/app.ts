667c3ae3b4600ad505cc43522caf40eb2b24f0b2
Michael Richardson <michael.richardson@octopus.com>
2025-07-10 22:03:03 +1000

    ejs templating

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import path from "node:path";
 
 const app = express();
 const port = 3000;
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.send('Hello World from TypeScript Web Server!');
+    res.render('index', { greeting: 'Hello' });
 });
 
 app.listen(port, () => {
